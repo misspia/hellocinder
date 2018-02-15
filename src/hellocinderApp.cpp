c0929474815f7f195518595773006edd9c5761ed
misspia <pia_leung@hotmail.com>
2018-02-14 21:02:44 -0800

    perspective camera and first animation

--- src/hellocinderApp.cpp
+++ src/hellocinderApp.cpp
@@ -27,11 +27,9 @@ void hellocinderApp::update()
 {
 }
 
-void hellocinderApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) );
+void testTransform() {
 	vec2 center = getWindowCenter();
-	
+
 	gl::pushModelMatrix();
 	gl::translate(center);
 
@@ -55,6 +53,49 @@ void hellocinderApp::draw()
 	gl::color(Color(1, 1, 1));
 	gl::drawSolidCircle(vec2(), 15);
 	gl::popModelMatrix();
+}
+
+void test3D() {
+	gl::enableDepthRead();
+	gl::enableDepthWrite();
+
+	CameraPersp cam;
+	cam.lookAt(vec3(3, 4.5, 4.5), vec3(0, 1, 0));
+	gl::setMatrices(cam);
+
+	auto lambert = gl::ShaderDef().lambert().color();
+	auto shader = gl::getStockShader(lambert);
+	shader->bind();
+
+	int numSpheres = 64;
+	float maxAngle = M_PI * 7;
+	float spiralRadius = 1;
+	float height = 2;
+	float anum = getElapsedFrames() / 30.0f;
+
+	for (int s = 0; s < numSpheres; s++) {
+		float rel = s / (float)numSpheres;
+		float angle = rel * maxAngle;
+		float y = fabs(cos(rel * M_PI + anum)) * height;
+		float r = rel * spiralRadius;
+		vec3 offset(r * cos(angle), y / 2, r * sin(angle));
+
+		gl::pushModelMatrix();
+		gl::translate(offset);
+		gl::scale(vec3(0.05f, y, 0.05f));
+		gl::color(Color(CM_HSV, rel, 1, 1));
+		gl::drawCube(vec3(), vec3(1));
+		gl::popModelMatrix();
+		//console() << gl::getProjectionMatrix() << std::endl;
+	}
+}
+
+void hellocinderApp::draw()
+{
+	gl::clear();
+	//testTransform();
+	test3D();
+	
 
 }
 
